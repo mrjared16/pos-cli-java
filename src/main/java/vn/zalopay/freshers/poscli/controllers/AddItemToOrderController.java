884117dde0b38f987495a4dedf85e55ba6575d80
phucdnt <phucdnt@vng.com.vn>
2022-05-23 21:08:12 +0700

    add controller to handle add item to order

---
+++ src/main/java/vn/zalopay/freshers/poscli/controllers/AddItemToOrderController.java
@@ -0,0 +1,59 @@
+package vn.zalopay.freshers.poscli.controllers;
+
+import vn.zalopay.freshers.poscli.controllers.handlers.ItemChoosingProcessor;
+import vn.zalopay.freshers.poscli.controllers.handlers.QuantityProcessor;
+
+
+public class AddItemToOrderController implements Controller {
+    private OrderItemBuilder orderItemBuilder;
+    private final OrderBuilder orderBuilder;
+    private final Controller predecessor;
+
+    public AddItemToOrderController(Controller predecessor, OrderBuilder orderBuilder) {
+        this.predecessor = predecessor;
+        this.orderBuilder = orderBuilder;
+        this.reset();
+    }
+
+    public void run() {
+        ItemChoosingProcessor itemChoosingProcessor = new ItemChoosingProcessor();
+        QuantityProcessor quantityProcessor = new QuantityProcessor();
+        QuantityProcessor.AddToppingProcessor addToppingProcessor = new QuantityProcessor.AddToppingProcessor();
+        itemChoosingProcessor.setNext(quantityProcessor);
+        quantityProcessor.setNext(addToppingProcessor);
+        itemChoosingProcessor.handle(orderItemBuilder);
+        // check valid id
+//        System.out.print("Enter the quantity: ");
+//        orderItemBuilder.setQuantity(input);
+//        // check valid quantity
+//        System.out.print("Enter the toppings (default is no topping): ");
+//        orderItemBuilder.setToppings(new ToppingItem[]{});
+        // check valid input
+//        System.out.print("Enter the note: ");
+        // check valid input
+        this.addOrderItem();
+        // back to create order screen
+        this.predecessor.run();
+    }
+
+    private void addOrderItem() {
+        orderBuilder.addOrderItem(orderItemBuilder.createOrderItem());
+        this.showSuccessMessage();
+        this.reset();
+    }
+
+    private void showSuccessMessage() {
+        System.out.println("Added " + orderItemBuilder.getMenuItem().getName() + " to order!");
+    }
+
+    @Override
+    public void reset() {
+        this.orderItemBuilder = new OrderItemBuilder();
+    }
+
+    @Override
+    public void showGreetingMessage() {
+        System.out.println();
+        System.out.println("Adding item to order...");
+    }
+}
