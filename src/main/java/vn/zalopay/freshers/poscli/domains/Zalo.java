806049e786c978f9a3cb602d0a500252f8d91c9a
phucdnt <phucdnt@vng.com.vn>
2022-05-24 16:27:29 +0700

    implement Zalo sender

---
+++ src/main/java/vn/zalopay/freshers/poscli/domains/Zalo.java
@@ -0,0 +1,18 @@
+package vn.zalopay.freshers.poscli.domains;
+
+public class Zalo implements Sender {
+    private final String phoneNumber;
+
+    public Zalo(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void send(Message messageBody) {
+        System.out.printf("Sending to Zalo username with %s...", this.phoneNumber);
+        System.out.println();
+        System.out.printf("Content: %s", messageBody.toString());
+        System.out.println();
+        System.out.println("Sent to Zalo!");
+    }
+}
